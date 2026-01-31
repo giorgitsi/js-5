825f0e00ae4948f05fc3dccb2c8f4b7ef7b37a9f
giorgitsi <113306383+giorgitsi@users.noreply.github.com>
2026-01-31 13:39:23 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+let randomNumber = Math.floor(Math.random() * 10) + 1;
+console.log("შემთხვევითი რიცხვი:", randomNumber);
+
+
+let number = 7.6;
+
+console.log(Math.round(number)); // უახლოეს მთელზე
+console.log(Math.floor(number)); // ქვემოთ დამრგვალება
+console.log(Math.ceil(number));  // ზემოთ დამრგვალება
+
+
+
+let a = 15;
+let b = 32;
+let c = 9;
+
+console.log("მაქსიმუმი:", Math.max(a, b, c));
+console.log("მინიმუმი:", Math.min(a, b, c));
+
+
+
+
+let x = 16;
+
+console.log("კვადრატული ფესვი:", Math.sqrt(x));
+console.log("ხარისხი:", Math.pow(x, 2));
